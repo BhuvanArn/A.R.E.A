d18780227fda59e30f8a7216b84124b61fce0d32
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-12-04 11:23:54 +0100

    :heavy_plus_sign: Added the base of the Area home page

---
+++ client/src/views/AreaHome.vue
@@ -0,0 +1,30 @@
+<template>
+    <NavBar></NavBar>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</template>
+
+<script>
+import NavBar from '../components/MainNavBar.vue';
+
+export default {
+    name: 'AreaHome',
+    data() {
+        return {
+            // data
+        }
+    },
+    components: {
+        NavBar
+    },
+    methods: {
+        // methods
+    }
+}
+</script>
+
+<style>
+</style>
