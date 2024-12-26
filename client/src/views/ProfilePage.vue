e3e1a1844bd7e2c162c145e294225cfce387bfb2
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-12-26 18:22:42 +0100

    :heavy_plus_sign: Added the base of the profile page

---
+++ client/src/views/ProfilePage.vue
@@ -0,0 +1,51 @@
+<template>
+    <body>
+        <NavBar class="navbar"></NavBar>
+        <div class="profile-page">
+
+        </div>
+    </body>
+</template>
+
+<script>
+import NavBar from '../components/WorkspaceNavBar.vue';
+
+export default {
+    name: "ProfilePage",
+    components: {
+        NavBar,
+    },
+    data() {
+        return {
+        };
+    },
+
+    mounted() {
+    },
+    methods: {
+    },
+
+};
+
+</script>
+
+<style>
+body {
+    background-color: #efefef;
+}
+
+.navbar {
+    position: fixed;
+    z-index: 10;
+}
+
+.profile-page {
+    position: relative;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    font-family: 'inter', sans-serif;
+    z-index: 1;
+}
+</style>
