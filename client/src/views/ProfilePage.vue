76589bb73d65b0e9649bf2934d76a7cfaf9302e4
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2024-12-29 20:14:36 +0100

    :heavy_plus_sign: Added the AccountSection cmp to the ProfilePage

--- client/src/views/ProfilePage.vue
+++ client/src/views/ProfilePage.vue
@@ -2,18 +2,23 @@
     <body>
         <NavBar class="navbar"></NavBar>
         <div class="profile-page">
+<!--             <div class="navigation-container">
 
+            </div> -->
+            <AccountSection></AccountSection>
         </div>
     </body>
 </template>
 
 <script>
 import NavBar from '../components/WorkspaceNavBar.vue';
+import AccountSection from '@/components/AccountSection.vue';
 
 export default {
     name: "ProfilePage",
     components: {
         NavBar,
+        AccountSection,
     },
     data() {
         return {
@@ -35,17 +40,30 @@ body {
 }
 
 .navbar {
-    position: fixed;
     z-index: 10;
 }
 
 .profile-page {
     position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 100vh;
     width: 100%;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     font-family: 'inter', sans-serif;
     z-index: 1;
+}
+
+.navigation-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 15rem;
+    min-height: 15rem;
+    background-color: transparent;
 }
 </style>
