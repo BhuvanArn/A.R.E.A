d9ec2b26bfa4db83c3be988981885fc4cc1ddca7
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2025-01-07 23:17:18 +0100

    :heavy_plus_sign: Initialization of dotenv

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default defineConfig({
   plugins: [vue()],
   server: {
