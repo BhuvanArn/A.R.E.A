2698e37642d7d57e1b6946b52dcaba55054be276
Fizo55 <hugomacees56000@gmail.com>
2025-01-13 02:43:24 +0100

    🔧fix build

--- services/advanced/ActionReactionService/GetServicesEventHandler.cs
+++ services/advanced/ActionReactionService/GetServicesEventHandler.cs
@@ -24,16 +24,26 @@ public class GetServicesEventHandler : IIntegrationEventHandler<GetServiceEvent,
             return (new(), ResultType.Fail);
         }
         
-        var services = await _dbHandler.GetAsync<Service>(s => s.UserId == userId);
+        var services = (await _dbHandler.GetAsync<Service>(s => s.UserId == userId)).ToList();
+
+        if (services.Count == 0)
+        {
+            return (new(), ResultType.Fail);
+        }
 
         List<Area> list = new();
         
         foreach (var service in services)
         {
+            var area = (await _dbHandler.GetAsync<Area>(s => s.ServiceId == service.Id)).FirstOrDefault();
+
+            if (area is null)
+            {
+                continue;
+            }
             
+            list.Add(area);
         }
-        
-        var area = await _dbHandler.GetAsync<Area>(s => s.ServiceId == )
         
         return (services.ToList(), ResultType.Success);
     }
