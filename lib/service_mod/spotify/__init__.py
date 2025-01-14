432c750302012303fe05b23ce28a1162a7726b73
Clément Baziret <clement.baziret@epitech.eu>
2025-01-14 23:21:53 +0100

    :recycle: Remove unused code and split playback action into 2 different action

--- lib/service_mod/spotify/__init__.py
+++ lib/service_mod/spotify/__init__.py
@@ -1,19 +1,19 @@
-from serviceconf import Action
 import json
 
-def middleware_playback_playing(register: object, action: Action, discard, valid, data = None):
+def strategy_playback_playing_continuously(register: object, data: bytes):
     if data:
         json_data = json.loads(data)
         if json_data["is_playing"] == True:
-            valid(json_data["is_playing"])
+            return ("playback_action_triggered")
+    return (None)
+
+def strategy_playback_playing_once(register: object, data: bytes):
+    if data:
+        json_data = json.loads(data)
+        if json_data["is_playing"] == True:
+            if not register.cache:
+                register.cache = "playback_action_triggered"
+                return ("playback_action_triggered")
         else:
             register.cache = None
-    valid(None)
-
-def strategy_playback_playing(register: object, data: bytes):
-    if (register.cache and data):
-        return (None)
-    if (data):
-        register.cache = data
-        return (True)
     return (None)
