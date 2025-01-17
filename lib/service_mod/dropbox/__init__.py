033faedbfba3820a104bbd49272e218ebf8b22ca
Clément Baziret <clement.baziret@epitech.eu>
2025-01-17 22:26:33 +0100

    :recycle: Removed unused middleware in dropbox file_search action

--- lib/service_mod/dropbox/__init__.py
+++ lib/service_mod/dropbox/__init__.py
@@ -1,22 +1,15 @@
 from serviceconf import Action
 import json
-import sys
 
-def middleware_file_search(register: object, action: Action, discard, valid, data = None):
+def strategy_file_search(register: object, data: bytes):
     searched_file = register.vars["file_name"]
     if data:
         json_data = json.loads(data)
         for match in json_data["matches"]:
             if match["metadata"]["metadata"]["name"] == searched_file:
-                valid(match["metadata"]["metadata"]["name"])
-                return
+                if not register.cache:
+                    register.cache = "file_found"
+                    return ("file_found")
+                return (None)
         register.cache = None
-    valid(None)
-
-def strategy_file_search(register: object, data: bytes):
-    if (register.cache and data):
-        return (None)
-    if (data):
-        register.cache = data
-        return (True)
     return (None)
