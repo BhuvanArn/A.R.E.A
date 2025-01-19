325f80132e79e06c8abd869e72833a6da62de432
Andriamanampisoa <toavina.andriamanampisoa@epitech.eu>
2025-01-19 01:33:36 +0100

    :sparkles: Add the website footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer>
+        <div class="footer-container">
+            <img src="@/assets/logo.png" alt="logo" width="30" height="30">
+            <p class="footer-txt"> © 2025 - All Rights Reserved </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+</script>
+
+<style>
+
+.footer-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2c3e50;
+    color: white;
+    height: 100px;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+}
+
+.footer-txt {
+    font-size: 1.2rem;
+    font-weight: 400;
+    font-family: 'inter', sans-serif;
+    color: rgb(255, 255, 255);
+    margin-left: 1rem;
+    margin-right: 1rem;
+}
+
+</style>
